21a28d67c23bb90de8626463bd2059bdbf99190c
Mhe115 <conorhealy490@gmail.com>
2024-10-18 18:01:04 +0100

    Testing the new library's features

--- project/template.java
+++ project/template.java
@@ -4,25 +4,62 @@ public class template
 {
      public static void main(String[] args)
      {
-          System.loadLibrary("sysinfo");
-          sysInfo info = new sysInfo();
-          //int     square = info.intExample(5);
-          //System.out.println("intExample: " + square);
-          //String     str = info.stringExample("test");
-          //System.out.println("stringExample: " + str);
-
-          cpuInfo cpu = new cpuInfo();
           String nameOS = "os.name";  
           String versionOS = "os.version";  
           String architectureOS = "os.arch";
           double memory = Runtime.getRuntime().totalMemory() / 1000000;
 
-          //System.out.println("CPU " + cpu.getModel() + " has "+ cpu.coreCount() + " cores");
+
+
+
+
+
+
+          System.loadLibrary("sysinfo");
+          sysInfo info = new sysInfo();
+
+          cpuInfo cpu = new cpuInfo();
+          cpu.read();
+
+          System.out.println("CPU " + cpu.getModel() + " has "+
+          cpu.socketCount() + " sockets each with "+
+          cpu.coresPerSocket() + " cores");
+          System.out.println("l1d="+cpu.l1dCacheSize()+
+          ", l1i="+cpu.l1iCacheSize()+
+          ", l2="+cpu.l2CacheSize()+
+          ", l3="+cpu.l3CacheSize());
+
+
+          /*while(true)
+          {
+            try {
+                Thread.sleep (1000);
+            }
+            catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                return;
+            }
+
+            cpu.read();
+            System.out.println("core 3 idle="+cpu.getIdleTime(3)+"%");
+          }*/
+
+
+
+
+
+
+
+          
+          
+
+          
           System.out.println("\nTotal memory available to JVM (Megabytes): " + memory );
           System.out.println("\nOS: \t" + System.getProperty(nameOS));
           System.out.println("OS Version: \t" + System.getProperty(versionOS));
           System.out.println("OS Architecture: \t" + System.getProperty(architectureOS));
-          //System.out.println(info);
+          System.out.println();
+          
 
           
      }
