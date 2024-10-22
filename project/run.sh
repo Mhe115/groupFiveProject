3b4a19b65c4f1d6137559e00b988269b72f13d38
Mhe115 <conorhealy490@gmail.com>
2024-10-22 10:40:22 +0100

    Adding JFreeChart Lib

--- project/run.sh
+++ project/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 make
 export _JAVA_OPTIONS="-Xms1024m -Xmx1024m"
-java -Djava.library.path=libsysinfo template 
+
+java -Djava.library.path=libsysinfo -cp "jFreeChartLib/jfreechart-1.5.3.jar:jFreeChartLib/jcommon-1.0.23.jar:." template
 
