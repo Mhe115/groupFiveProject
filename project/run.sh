cf5d444216924a96e5714683e174a70d0d7a664e
Mhe115 <conorhealy490@gmail.com>
2024-10-24 14:52:50 +0100

    Added install maven to run.sh

--- project/run.sh
+++ project/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 make
 export _JAVA_OPTIONS="-Xms1024m -Xmx1024m"
-
+sudo apt install maven
 java -Djava.library.path=libsysinfo -cp "jFreeChartLib/jfreechart-1.5.3.jar:jFreeChartLib/jcommon-1.0.23.jar:." template
 
